c273901f9b0bcc4a227c59b7706a0ddb7c32a43e
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-12-05 16:44:18 +0100

    added gif slider

--- app.js
+++ app.js
@@ -8,8 +8,88 @@ let snow = 100;
 for(let i = 0; i < snow; i++ ){
 
   let moreflake = flake.cloneNode(true);
-  moreflake.style.paddingLeft = Math.random() * 20 + "px";
-  moreflake.style.animationDuration = Math.random() * 10 + 16 + "s";
+  moreflake.style.opacity = Math.random() * 1;
+  moreflake.style.animationDuration = Math.random() * 20 + 30 + "s";
   container.appendChild(moreflake); 
   
 };
+
+
+
+const gifs = [
+  'https://media.giphy.com/media/ienko7tUwlgz86igqU/giphy.gif',
+  'https://media.giphy.com/media/r3jTnU6iEwpbO/giphy.gif',
+  'https://media.giphy.com/media/K2l8gFcatHmsE/giphy.gif',
+  
+];
+ 
+const slides = document.querySelector(".slider_slide");
+const forwardButton = document.querySelector(".slider_nav_forward");
+const backButton = document.querySelector(".slider_nav_back");
+
+
+slides.style.display = "block";
+
+
+var currentIndex = 0;
+var newIndex = 0; // keep track of the new slide
+
+
+function navigateSlider() {
+   slides.src = gifs[newIndex];
+   currentIndex = newIndex;
+   forwardButton.disabled = (newIndex === gifs.length - 1) ? true : false;
+   backButton.disabled = (newIndex === 0) ? true : false;
+}
+
+backButton.addEventListener('click', () => {
+  newIndex--;
+  navigateSlider();
+});
+
+forwardButton.addEventListener('click', () => {
+  newIndex++;
+  navigateSlider();
+});
+
+   
+
+
+
+/*
+function navigateSlider() {
+  slides.src = gifs[newIndex];
+  forwardButton.disabled = (newIndex === gifs.length - 1) ? true : false;
+  backButton.disabled = (newIndex === 0) ? true : false;
+}
+
+
+forwardButton.addEventListener('click', () => {
+  newIndex = (newIndex !== gifs.length - 1) ? newIndex + 1 : 0;
+  navigateSlider();
+  
+  if(newIndex < gifs.length - 1){
+    newIndex++;
+  }
+  else{
+    index = 0; 
+  }
+});
+
+backButton.addEventListener('click', () => {
+  navigateSlider();
+  if (newIndex == 0){
+    newIndex = gifs.length - 1;
+} 
+else{
+  newIndex = newIndex - 1;
+} 
+  
+});
+
+
+*/
+
+
+
+
