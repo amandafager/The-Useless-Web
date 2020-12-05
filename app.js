2ccb74a3e0c4c7f5edf34b9b14b2f10a35c6930a
amanda.fager@outlook.com <amanda.fager@outlook.com>
2020-12-05 22:50:58 +0100

    test swipe

--- app.js
+++ app.js
@@ -54,7 +54,39 @@ forwardButton.addEventListener('click', () => {
 
 const card = document.querySelector(".card");
 
+const _C = document.querySelector('.img-container'), 
+      N = _C.children.length;
 
+_C.style.setProperty('--n', N)
 
+function lock(e) {};
 
+function move(e) {};
+
+_C.addEventListener('mousedown', lock, false);
+_C.addEventListener('touchstart', lock, false);
+
+_C.addEventListener('mouseup', move, false);
+_C.addEventListener('touchend', move, false);
+
+function unify(e) { return e.changedTouches ? e.changedTouches[0] : e };
+
+let x0 = null;
+
+function lock(e) { x0 = unify(e).clientX };
+
+let i = 0;
+
+function move(e) {
+  if(x0 || x0 === 0) {
+    let dx = unify(e).clientX - x0, s = Math.sign(dx);
+  
+    if((i > 0 || s < 0) && (i < N - 1 || s > 0))
+      _C.style.setProperty('--i', i -= s);
+	
+    x0 = null
+  }
+};
+
+_C.addEventListener('touchmove', e => {e.preventDefault()}, false);
 
